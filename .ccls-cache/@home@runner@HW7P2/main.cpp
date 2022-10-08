6d79ccbd284364dd6b976a84245003b5ac96ddcd
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-08 03:11:18 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW7P2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+void time(int seconds, int hours, int mins, int secs){
+cin >> seconds;
+hours = 
+mins = 
+secs = 
+
+
+  
+  cout << hours << "h, " << mins << "m, " << secs << "s" << endl;
+}
+
+int main() {
+int seconds_in;
+int h;
+int m;
+int s;
+
+  
+time(seconds_in, h, m, s);
+}
