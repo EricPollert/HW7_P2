42afbf271e8d0acb51c0fded4cba8f696cbcf1de
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-08 03:47:13 +0000

    Revised the equations for seconds, minutes and hours to run correctly. Changed cout statement for better clarity

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void time(int seconds, int hours, int mins, int secs){
 cin >> seconds;
-cout << "Enter number of seconds" << endl;
-hours = (seconds/3600);
-mins = (seconds/60);
-secs = seconds;
+cout << "Enter number of seconds: " << endl;
+hours = (seconds/3600) % 60;
+mins = (seconds/60) % 60;
+secs = seconds % 60;
 
 
   
-  cout << hours << "h, " << mins << "m, " << secs << "s" << endl;
+  cout << hours << " hours, " << mins << " minutes, " << secs << " seconds" << endl;
 }
 
 int main() {
